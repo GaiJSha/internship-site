003ad532fade1ae3fa311a3e9027e54322fa361e
gai sha <oohiiv1@gmail.com>
2020-04-07 18:55:52 +0300

    added siple components and a little css

---
+++ worker-site/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface HeaderProps {
+    
+}
+ 
+const Header: React.FC<HeaderProps> = () => {
+    return <header>
+        
+    </header>;
+}
+ 
+export default Header;
